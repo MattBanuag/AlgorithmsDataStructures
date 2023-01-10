5038720f71e94a406e24671bf45b0bbc015e536e
MattBanuag <matthewbanuag.dev@gmail.com>
2023-01-09 19:55:18 -0600

    feat: prompts

--- LAB_01/LAB_01/Program.cs
+++ LAB_01/LAB_01/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// How many words - PROMPT
+Console.WriteLine("How many words would you like to check?");
+int wordCount = Int32.Parse(Console.ReadLine());
+
+// What are the words - PROMPT
+Console.WriteLine("Which words would they be?");
+Console.WriteLine("NOTE: Please press <enter> after every word.");
+// Initializing array and passing values
+string[] wordsArray = new string[wordCount];
+for (int i = 0; i < wordCount; i++)
+{
+    wordsArray[i] = Console.ReadLine();
+}
+
+// Entering a character - PROMPT
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Please enter a character:");
+char character = Char.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
