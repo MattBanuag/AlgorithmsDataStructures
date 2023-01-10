d0c8c064f1081fdb3e52ec212c07d4e27f4ef65c
MattBanuag <matthewbanuag.dev@gmail.com>
2023-01-10 07:31:49 -0600

    fix: calculation bugs

--- LAB_01/LAB_01/Program.cs
+++ LAB_01/LAB_01/Program.cs
@@ -11,12 +11,47 @@ string[] wordsArray = new string[wordCount];
 for (int i = 0; i < wordCount; i++)
 {
     wordsArray[i] = Console.ReadLine();
+
+    // Validation
+    if (wordsArray[i].Contains(" "))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("ERROR: Words cannot contain a space. Hit <enter> to try again.");
+        Console.ReadLine();
+        return;
+    }
 }
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine();
 
 // Entering a character - PROMPT
 Console.WriteLine("~ Please enter a character:");
 char character = Console.ReadKey().KeyChar;
+int inputTries = 5;
+
+// Validation
+do
+{
+    if (!Char.IsLetter(character) && inputTries > 0)
+    {
+        inputTries--;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine();
+        Console.WriteLine($"ERROR: '{character}' is not a letter. Please select a letter: ");
+        character = Console.ReadKey().KeyChar;
+    }
+    else if (inputTries == 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"ERROR: You have reached the maximum amount of tries. Hit <enter> to try again");
+        Console.ReadLine();
+        return;
+    } else
+    {
+        inputTries = -1;
+    }
+} while (inputTries > -1);
+
 int occurenceCount = 0;
 int totalCharCount = 0;
 Console.WriteLine();
@@ -34,7 +69,7 @@ for(int i = 0; i < wordsArray.Length; i++)
 }
 
 // Result Summary
-double characterPercentage = ((double)wordCount / (double)totalCharCount) * 100;
+double characterPercentage = ((double)occurenceCount / (double)totalCharCount) * 100;
 Console.WriteLine();
 Console.WriteLine($"~ '{character}' is seen {occurenceCount} times in the array");
 Console.WriteLine($"~ '{character}' makes up {characterPercentage}% of the array");
