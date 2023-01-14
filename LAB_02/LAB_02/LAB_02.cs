5680e04988b0caf796218f09ed2fa47d9934f1bf
MattBanuag <matthewbanuag.dev@gmail.com>
2023-01-14 08:19:36 -0600

    build: finished program 2

--- LAB_02/LAB_02/LAB_02.cs
+++ LAB_02/LAB_02/LAB_02.cs
@@ -5,7 +5,10 @@
     For example, the string “Programmatic Python” would result in 
     the array ['p','r','o','a','m','t'].
 */
-Console.WriteLine("~ Please enter a word of at LEAST 15 characters: ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("~ Getting repeated characters in a string: ");
+Console.WriteLine("~ Please enter 2 words with repeated characters: ");
+Console.ForegroundColor = ConsoleColor.White;
 string userInput = Console.ReadLine();
 string formattedInput = userInput.ToLower();
 char[] charArray = formattedInput.ToCharArray();
@@ -16,7 +19,7 @@ for (int i = 0; i < charArray.Length; i++)
     int indexOfFirst = Array.IndexOf(charArray, charArray[i]);
     int indexOfSecond = Array.LastIndexOf(charArray, charArray[i]);
 
-    if(indexOfFirst != indexOfSecond)
+    if (indexOfFirst != indexOfSecond)
     {
         repeatingChars += charArray[i];
     }
@@ -37,62 +40,64 @@ foreach (char c in resultRepeatedChars)
 }
 
 Console.WriteLine();
+
 /*
     A program returns an array of strings that are 
     unique words found in the argument.
 
     For example, the string “To be or not to be” 
     returns ["to","be","or","not"].
-*//*
+*/
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("~ Getting unique words from a string: ");
 Console.WriteLine("~ Please enter a sentence with some repeated words: ");
+Console.ForegroundColor = ConsoleColor.White;
 string userInputString2 = Console.ReadLine();
 string formattedString = userInputString2.ToLower();
 string[] testStrings = formattedString.Split(" ");
 string uniqueStrings = "";
 
-for(int i = 0; i < testStrings.Length; i++)
+for (int i = 0; i < testStrings.Length; i++)
 {
     int indexOfFirst = Array.IndexOf(testStrings, testStrings[i]);
     int indexOfSecond = Array.LastIndexOf(testStrings, testStrings[i]);
 
     // Repeeating Strings
-    if (indexOfFirst != indexOfSecond)
+    if (uniqueStrings.IndexOf(testStrings[i]) == -1)
     {
-        testStrings[indexOfSecond].Remove(0, testStrings[indexOfSecond].Length);
-        uniqueStrings += testStrings[i];
-    } else
-    {
-        // Unique Strings
-        uniqueStrings += testStrings[i];
+        uniqueStrings += testStrings[i] + ",";
     }
 }
 
-string[] uniqueStringsArray = uniqueStrings.Split(" "); 
+string[] uniqueStringsArray = uniqueStrings.Split(",");
 
-foreach(string str in uniqueStringsArray)
+foreach (string str in uniqueStringsArray)
 {
-    Console.WriteLine("~ " + str);
+    Console.Write($"[{str}] ");
 }
 
 Console.WriteLine();
 
-*//*
+/*
     A program that reverses a provided string  
 */
-/*Console.WriteLine("~ Reversing string");
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("~ Reversing string");
 Console.WriteLine("~ Please enter a word: ");
+Console.ForegroundColor = ConsoleColor.White;
 string reversedWord = "";
 string userInputString3 = Console.ReadLine();
 char[] charsList3 = userInputString3.ToCharArray();
 
-for(int i = charsList3.Length - 1; i >= 0; i--)
+for (int i = charsList3.Length - 1; i >= 0; i--)
 {
     reversedWord += charsList3[i];
 }
 
 Console.WriteLine(reversedWord);
-Console.WriteLine();*/
+Console.WriteLine();
 
 /*
     A program that finds the longest unbroken word 
@@ -102,8 +107,10 @@ Console.WriteLine();*/
     would print "through" If there are multiple words tied 
     for greatest length, print the last one
 */
-/*Console.WriteLine("~ Longest unbroken word");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("~ Longest unbroken word");
 Console.WriteLine("~ Please enter a sentence: ");
+Console.ForegroundColor = ConsoleColor.White;
 string userInputString4 = Console.ReadLine();
 userInputString4.ToLower();
 string[] wordsArray = userInputString4.Split(" ");
@@ -111,7 +118,7 @@ string[] wordsArray = userInputString4.Split(" ");
 string longestWord = "";
 int nextIndex = 1;
 
-for(int i = 0; i < wordsArray.Length; i++)
+for (int i = 0; i < wordsArray.Length; i++)
 {
     if (wordsArray[i].Length >= wordsArray[nextIndex].Length)
     {
@@ -119,4 +126,4 @@ for(int i = 0; i < wordsArray.Length; i++)
     }
 }
 
-Console.WriteLine(longestWord);*/
+Console.WriteLine(longestWord);
